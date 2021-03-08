a2c4fcbd554ca9864e5117870b3f77ec7725df49
AmirO8 <AmirO8@users.noreply.github.com>
2021-03-07 22:47:09 -0800

    update to read me

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,6 +14,6 @@ lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mea
 t.test(log10(Suspension_Coil$PSI),mu=mean(log10(Suspension_Coil$PSI))) 
 
 # Deliverable 3 Part 2
-Lot_1 <- t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot1', mu=mean(1500)))
-Lot_2 <- t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot2'))
-Lot_3 <- t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot3'))
+Lot_1 <- t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot1', ))
+Lot_2 <- t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot2', ))
+Lot_3 <- t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot3', ))
