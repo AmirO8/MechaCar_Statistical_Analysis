3531716fa17623028fb40482e248ebe1a7e6ee3a
AmirO8 <AmirO8@users.noreply.github.com>
2021-03-07 18:56:39 -0800

    D3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,10 @@ Suspension_Coil <- read.csv(file = "Suspension_Coil.csv", header = TRUE)
 total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance = var(PSI), SD=sd(PSI))
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance = var(PSI), SD=sd(PSI), .groups = 'keep')
 
+# Deliverable 3
+t.test(log10(Suspension_Coil$PSI),mu=mean(log10(Suspension_Coil$PSI))) 
+
+# Deliverable 3 Part 2
+Lot_1 <- t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot1', mu=mean(1500)))
+Lot_2 <- t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot2'))
+Lot_3 <- t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot3'))
